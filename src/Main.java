1b16b602acd4e9a6c3f47373a9937bc481338ce2
matkinson <matkinson@USA-MAC-NIS1899.local>
2021-07-21 11:35:10 -0700

    7/21/21 final commit on markWork2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,20 +9,19 @@ public class Main {
         double studentGpa = 3.45;
         String studentFirstName = "Mark";
         String studentLastName = "Atkinson";
-        //char studentFirstInitial = 'M';
         char studentFirstInitial = studentFirstName.charAt(0);
-        //char studentLastInitial = 'A';
         char studentLastInitial = studentLastName.charAt(0);
         boolean hasPerfectAttendance = true;
 
-        System.out.println(studentAge);
-        System.out.println(studentGpa);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
-     //   System.out.println(studentFirstName);
-     //   System.out.println(studentLastName);
-        System.out.println(studentFirstName + " " + studentLastName + " " + "has a GPA of: " + studentGpa);
+      //  System.out.println(studentFirstName);
+      //  System.out.println(studentLastName);
+        System.out.println(studentFirstName + " " + studentLastName + " " +
+                "has a GPA of: " + studentGpa);
+        System.out.println("What do you want to update it to?");
+        Scanner input = new Scanner(System.in);
+        studentGpa = input.nextDouble();
+        System.out.println(studentFirstName + " " + studentLastName + " " +
+                "Now has a GPA of: " + studentGpa);
     }
 }
 
