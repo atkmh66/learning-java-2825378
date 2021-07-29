4286386b2a89645b3ee4a7cb143e49acdf6fad27
matkinson <matkinson@usa-mac-nis1899.local.gap.com>
2021-07-28 23:39:36 -0700

    7/28/21 initial capture markWork5.2

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        System.out.println("Let's calculate the area of a triangle");
-
+    public static void announceDeveloperTeaTime(){
+        System.out.println("Waiting for developer tea time ...");
+        System.out.println("Type in a random work and press Enter to start developer Tea Time");
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Please input the base of the triangle (in inches).");
-        //double base = input.nextDouble()
-        double base = input.nextDouble();
-
-        while (base <= 0) {
-            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            base = input.nextDouble();
-        }
-
-        System.out.println("Please input the height of the triangle (in inches).");
-        double height = input.nextDouble();
-        while (height <= 0) {
-            //System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            System.out.println("That's invalid. Please input the height of the triangle (in inches).");
-           // base = input.nextDouble();
-            height = input.nextDouble();
-        }
-
-        double area = (base * height) / 2;
-        //System.out.println("The area is " + height);
-        System.out.println("The area is " + area);
-
+        input.next();// Without making an assignment, what ever we type and press enter will allow continuation
+        System.out.println("It's developer Tea Time");
     }
+    public static void main(String[] args) {
+        announceDeveloperTeaTime();
+    }
+
 }
