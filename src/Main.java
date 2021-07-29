6c023e96dfeaf9eefdda09f6a7ab5cbdfa69c1fe
matkinson <matkinson@usa-mac-nis1899.local.gap.com>
2021-07-28 23:12:15 -0700

    7/28/21 Final chapter capture

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,34 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner inputScaner = new Scanner(System.in);
-        String question = "What is the answer to Life, the Universe, and Everything";
-        String choiceOne = "work";
-        //String choiceTwo = "42";
-        String choiceTwo = "jupiter";
-        String choiceThree = "mice";
 
-        String correctAnswer= choiceTwo;
-        String inputAnswer = "";
+        System.out.println("Let's calculate the area of a triangle");
 
-        System.out.println(question);
-        System.out.println("Possible choices are: "+ choiceOne + ", " + choiceTwo + ", " + choiceThree);
-        inputAnswer = inputScaner.next();
-//        System.out.println(inputAnswer); // debug code
-//        System.out.println(correctAnswer); // debug code
+        Scanner input = new Scanner(System.in);
 
-        if (correctAnswer.equals( inputAnswer.toLowerCase() )) {// can also use inputAnswer.toLowerCase() allows all input
-                                                  // if all the answers were defined as lowwer case to start with
-            System.out.println("That Is Correct.  Well Done !!");
-        } else {
-            System.out.println("That answer was incorrect.");
-            System.out.println("The correct answer is: " + correctAnswer );
+        System.out.println("Please input the base of the triangle (in inches).");
+        //double base = input.nextDouble()
+        double base = input.nextDouble();
+
+        while (base <= 0) {
+            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
+            base = input.nextDouble();
         }
+
+        System.out.println("Please input the height of the triangle (in inches).");
+        double height = input.nextDouble();
+        while (height <= 0) {
+            //System.out.println("That's invalid. Please input the base of the triangle (in inches).");
+            System.out.println("That's invalid. Please input the height of the triangle (in inches).");
+           // base = input.nextDouble();
+            height = input.nextDouble();
+        }
+
+        double area = (base * height) / 2;
+        //System.out.println("The area is " + height);
+        System.out.println("The area is " + area);
+
     }
 }
-
-
-
-
-
-
-
