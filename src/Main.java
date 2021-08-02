bf74767b8018c2499c39fc7b41fa3c72fb12c164
matkinson <matkinson@USA-MAC-NIS1899.local>
2021-08-01 21:09:09 -0700

    8/1/21 first last

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void calculatetotalmealprice( double listedMealPrice,
+    public static double calculateTotalMealPrice( double listedMealPrice,
                                                 double tipRate,
                                                 double texRate) {
         double tip = tipRate * listedMealPrice;
         double tax = texRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
-        System.out.println("Your total meal price is " + result);
+        return result;
+//        System.out.println("Your total meal price is " + result);
 
     }
     public static void main(String[] args) {
-        calculatetotalmealprice(15, 0.2, 0.08);
-        calculatetotalmealprice(25, 0.18, 0.08);
+        double groupTotalMealPrice = calculateTotalMealPrice(100, 0.2, 0.08);
+        System.out.println(groupTotalMealPrice);
 
-
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
     }
 
 }
